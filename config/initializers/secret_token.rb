1a18154063def0d78e0dfb81487cf0f7936a48dd
Joe Hand <hand@Joe-Hands-iMac.local>
2012-11-02 20:29:39 -0700

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowTell::Application.config.secret_token = '38d8e3ac9b9d179204eb7cd3b9c8853c82bc8c121ee83a62c3f0bea902e1c3105eb8962610cf5cb8666c6cd24887fcbb3c0294869ad4c52997be79e661ca0de5'
